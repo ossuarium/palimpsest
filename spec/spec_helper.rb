02fea28e0446c7db1a193584daf370f314b3b197
Evan Sosenko <razorx@evansosenko.com>
2013-09-25 16:11:35 -0700

    Added Coveralls::SimpleCov::Formatter.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ require 'palimpsest'
 require 'coveralls'
 require 'simplecov'
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
 SimpleCov.start
 
 RSpec.configure do |c|
